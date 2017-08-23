4905d382d280b4027ff35c329c4a2ce37b543e84
Max <maxwass@seas.upenn.edu>
2017-08-23 00:17:19 -0400

    building print_bits function. not working

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+void print_bits_of_int(int size, int num){
+//size is number of bytes in the integer
+//num is the value of the integer
+
+    char* bits[size*8];
+    
+    int is_even = (0 == (num % 2));
+
+
+}
+
+int main(void)
+{
+
+    int sign;
+
+    //bit shifting signed int is implimentation specific
+    //lets play around...
+    __int32_t fun;
+    
+
+
+
+}
