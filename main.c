6e55aa9bdf7b2fc2996abab99b7617e287d58eb5
Max <maxwass@seas.upenn.edu>
2017-08-23 01:09:22 -0400

    finished is_even function

--- main.c
+++ main.c
@@ -1,15 +1,25 @@
-#include <stdio.h>
 
+#include <stdio.h>
+#include <assert.h>
+
+int is_even(int num){
+    //returns 0 if even, -1 if odd
+    int remainder = num % 2;
+    if (remainder != 0) return -1;
+    return 0;
+}
 
 void print_bits_of_int(int size, int num){
 //size is number of bytes in the integer
 //num is the value of the integer
 
+    assert(&num != NULL && "null input into print_bits");
+    assert(size > 0 && "zero/neg sized integer input");
+
     char* bits[size*8];
+    int even = is_even(num);
+
     
-    int is_even = (0 == (num % 2));
-
-
 }
 
 int main(void)
@@ -19,9 +29,7 @@ int main(void)
 
     //bit shifting signed int is implimentation specific
     //lets play around...
-    __int32_t fun;
+    __int8_t fun = 4;  /* 0 0 0 0  0 1 0 0 */
     
-
-
 
 }
